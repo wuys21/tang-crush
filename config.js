71fae27ab3966170646ac2ff7b6d7f5b58567c3c
wuys21 <87468049+wuys21@users.noreply.github.com>
2021-07-15 15:09:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     desc: 'tại sao em lại kêu anh là bede? ',
     btnYes: 'thì anh xin trả lời,vì anh thích MALE <33',
     btnNo: 'vì trong đó có E và lật ngược nó lại <3 ,
-    question:'Trên thế giới này có hơn 7 tỉ nụ cười <3',
+    question:'trên thế giới này có hơn 7 tỉ nụ cười <3',
     btnReply: 'nhưng mà với anh,nụ cười của em mới là đẹp nhất <3',
     reply: 'thế nên là làm ơnnnnnnn! <33333333',
     mess: 'hãy để anh giúp em giữ nụ cười của em trên môi!'
