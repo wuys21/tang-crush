f2fc3755521a6f25d04e5d3f6ec0bbc423b4b6f7
wuys21 <87468049+wuys21@users.noreply.github.com>
2021-07-15 15:13:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     mặt trăng for the night
     còn lam 
     forever
-    btnIntro: 'kkk',
-    title: 'okay anh đã trằn trọc suốt đêm qua để đặt ra một câu hỏi là?',
-    desc: 'tại sao em lại kêu anh là bede? ',
-    btnYes: 'thì anh xin trả lời,vì anh thích MALE <33',
-    btnNo: 'vì trong đó có E và lật ngược nó lại <3 ,
-    question:'trên thế giới này có hơn 7 tỉ nụ cười <3',
-    btnReply: 'nhưng mà với anh,nụ cười của em mới là đẹp nhất <3',
-    reply: 'thế nên là làm ơnnnnnnn! <33333333',
+    mess: 'kkk',
+    mess: 'okay anh đã trằn trọc suốt đêm qua để đặt ra một câu hỏi là?',
+    mess: 'tại sao em lại kêu anh là bede? ',
+    mess: 'thì anh xin trả lời,vì anh thích MALE <33',
+    mess: 'vì trong đó có E và lật ngược nó lại <3 ,
+    mess:'trên thế giới này có hơn 7 tỉ nụ cười <3',
+    mess: 'nhưng mà với anh,nụ cười của em mới là đẹp nhất <3',
+    mess: 'thế nên là làm ơnnnnnnn! <33333333',
     mess: 'hãy để anh giúp em giữ nụ cười của em trên môi!'
